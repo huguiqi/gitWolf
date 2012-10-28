b08b878c4a351e84dc251ac6a01822d97d4377dd
huguiqi <sam@huguiqitekiMacBook-Pro.local>
2012-10-28 18:58:44 +0800

    Initial Commit

---
+++ WolfShow/WFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WFAppDelegate.h
+//  WolfShow
+//
+//  Created by huguiqi on 12-10-28.
+//  Copyright (c) 2012年 huguiqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WolfShow/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WolfShow
+//
+//  Created by huguiqi on 12-10-28.
+//  Copyright (c) 2012年 huguiqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WFAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WFAppDelegate class]));
+    }
+}
